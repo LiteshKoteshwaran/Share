6ee60791178197ab8fd3ae5dd0fac9be4476fd4f
Litesh Koteshawaran <44187300+LiteshKoteshwaran@users.noreply.github.com>
2019-03-28 23:24:16 +0530

    initial commit

---
+++ MyBasket/Models/MyBasketRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBasket.Models
+{
+    public class MyBasketRepository
+    {
+
+        MyBasketDataContext _myBasketDataContext = new MyBasketDataContext();
+
+        internal List<Product> GetProducts()
+        {
+            return _myBasketDataContext.Products.ToList();
+        }
+
+        internal List<Brand> GetBrands()
+        {
+            return _myBasketDataContext.Brands.ToList();
+        }
+
+        internal List<Category> GetCategories()
+        {
+            return _myBasketDataContext.Categories.ToList();
+        }
+
+        internal List<Product> GetProductsByBrandId(int BrandId)
+        {
+            return _myBasketDataContext.Products.ToList().FindAll(product => product.BrandID == BrandId);
+        }
+
+        internal List<Product> GetProductsByCategoryId(int CategoryId)
+        {
+            return _myBasketDataContext.Products.ToList().FindAll(product => product.CategoryID == CategoryId);
+        }
+
+        internal Product GetProductById(int ProductId)
+        {
+            return _myBasketDataContext.Products.ToList().Find(product => product.ID == ProductId);
+        }
+
+    }
+}
---
+++ MyBasket/Models/MyCartRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MyBasket.Models
+{
+    public class MyCartRepository
+    {
+        MyBasketDataContext myBasketDataContext = new MyBasketDataContext();
+
+        public void AddToCart(int ProductID)
+        {
+            Product MyCartproduct = myBasketDataContext.Products.ToList().Find(product => product.ID == ProductID);
+        }
+
+    }
+}
